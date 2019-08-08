1a45716c5d3eb9f934be9ada004c17cd6c1d9995
Taylor Salo <tsalo006@fiu.edu>
2019-08-08 15:17:42 -0400

    Update environment.

--- generate_environment.sh
+++ generate_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 module add miniconda/2.7
-conda create -python=3.6 -p /home/data/cis/training-week-2019/env pip
+conda create -p /home/data/cis/training-week-2019/env python=3.6 pip -yq
 source activate /home/data/cis/training-week-2019/env
-conda install numpy libgfortran sympy ply funcsigs cython ipython ipython-notebook matplotlib seaborn pandas numexpr scikit-learn tornado accelerate Biopython dateutil
-pip install nibabel pyyaml ipywidgets
+conda install numpy libgfortran sympy ply funcsigs cython ipython ipython-notebook matplotlib seaborn pandas numexpr scikit-learn tornado accelerate Biopython dateutil pyyaml ipywidgets -yq
+pip install nibabel niwidgets pybids
 pip install https://github.com/nipy/nipy/archive/master.zip
 pip install https://github.com/moloney/dcmstack/archive/master.zip
 pip install nipype
